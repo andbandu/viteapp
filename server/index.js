1334305c63d98641108912cabcaa4d181fcdf4f0
andbandu <andbanduln@gmail.com>
2025-03-07 17:47:58 +0530

    Initial commit: Vite + React + Express setup

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/api', (req, res) => {
-  res.json({ message: 'Hello from Express!' });
+  res.json({ message: 'Hello from Express! game on baby' });
 });
 
 const PORT = process.env.PORT || 3001;
